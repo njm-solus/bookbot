8962bf0bbc9ac0303aad5665c7913da4b75a072b
njm-solus <njm.solus@gmail.com>
2025-05-17 18:16:18 +0300

    Add reading function

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print('greetings boots')
+def get_book_text(filename):
+    with open(filename) as f:
+        file_content = f.read()
+    return file_content
+
+
+
+def main():
+    #filename = input('Enter filename: ',)
+    return get_book_text('books/frankenstein.txt')
+
+print(main())
