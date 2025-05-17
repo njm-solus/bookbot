88808c550ba6d4d3d3ef7405e63fb9e87bd11eef
njm-solus <njm.solus@gmail.com>
2025-05-17 18:24:13 +0300

    Count number of words

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 def get_book_text(filename):
     with open(filename) as f:
         file_content = f.read()
-    return file_content
+        num_words = len(file_content.split())
+    return num_words
 
 
 
@@ -9,4 +10,6 @@ def main():
     #filename = input('Enter filename: ',)
     return get_book_text('books/frankenstein.txt')
 
-print(main())
+
+
+print(f'{main()} words found in the document')
