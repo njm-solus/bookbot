79539e84a5eb1591c9b1a9afc85c2b96ce889d50
njm-solus <njm.solus@gmail.com>
2025-05-17 21:02:11 +0300

    Add separate script to do functions

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
+from stats import get_num_words
+
+
 def get_book_text(filename):
     with open(filename) as f:
         file_content = f.read()
-        num_words = len(file_content.split())
-    return num_words
+    return file_content
 
 
 
 def main():
     #filename = input('Enter filename: ',)
-    return get_book_text('books/frankenstein.txt')
+    content = (get_book_text('books/frankenstein.txt'))
+    words = get_num_words(content)
+    return words 
 
 
 
---
+++ stats.py
@@ -0,0 +1,3 @@
+def get_num_words(file_content):
+    num_words = len(file_content.split())
+    return num_words
