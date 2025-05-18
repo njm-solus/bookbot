143d307d121da5f8eb30c879fcb089b5a2a533b2
njm-solus <njm.solus@gmail.com>
2025-05-18 12:47:52 +0300

    Modify main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from stats import get_num_words
-
+from stats import get_char_apps
 
 def get_book_text(filename):
     with open(filename) as f:
-        file_content = f.read()
+        file_content = f.read().lower()
     return file_content
 
 
@@ -12,8 +12,9 @@ def main():
     #filename = input('Enter filename: ',)
     content = (get_book_text('books/frankenstein.txt'))
     words = get_num_words(content)
-    return words 
+    appearances = get_char_apps(content)
+    return words, appearances
 
+num_words, num_appearances = main()
 
-
-print(f'{main()} words found in the document')
+print(f'{num_words} words found in the document and {num_appearances}')
