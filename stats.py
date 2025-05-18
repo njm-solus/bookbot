7949a0d770d125c47e7bc3d0c6fb432ef129f0aa
njm-solus <njm.solus@gmail.com>
2025-05-18 12:48:59 +0300

    Modify stats.py

--- stats.py
+++ stats.py
@@ -6,9 +6,7 @@ def get_num_words(file_content):
 
 def get_char_apps(file_content):
     src = "abcdefghijklmnopqrstuvwxyz0123456789\\][\"\',.;:|=+-_)(*&/^%$#@!~` "
-    b = int(0)
     apps_count = {}
-    #apps = file_content
     for char in src:
         apps_count[char] = file_content.count(char)
     return apps_count
