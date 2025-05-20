f826049b41fe7ded03b7a3a60378691f31111a08
njm-solus <njm.solus@gmail.com>
2025-05-20 10:51:39 +0300

    .

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from stats import get_num_words
 from stats import get_char_apps
+from stats import sort
+import sys
+
 
 def get_book_text(filename):
     with open(filename) as f:
@@ -9,12 +12,26 @@ def get_book_text(filename):
 
 
 def main():
-    #filename = input('Enter filename: ',)
-    content = (get_book_text('books/frankenstein.txt'))
+    if not len(sys.argv) == 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        filepath = sys.argv[1]
+    content = (get_book_text(filepath))
     words = get_num_words(content)
     appearances = get_char_apps(content)
-    return words, appearances
+    sorted = sort(appearances)
+    return filepath, words, appearances, sorted
 
-num_words, num_appearances = main()
+filepath, num_words, num_appearances, sorted = main()
 
-print(f'{num_words} words found in the document and {num_appearances}')
+#print(f'{num_words} words found in the document and {sorted}') #'''for key,value in num_appearances:'''}')
+print('============ BOOKBOT ============')
+print(f'Analyzing book found at {filepath}...')
+print('----------- Word Count ----------')
+print(f'Found {num_words} total words')
+print('--------- Character Count -------')
+for key, value in sorted.items():
+    if key.isalpha():
+        print(f'{key}: {value}')
+print('============= END ===============')
--- stats.py
+++ stats.py
@@ -9,4 +9,18 @@ def get_char_apps(file_content):
     apps_count = {}
     for char in src:
         apps_count[char] = file_content.count(char)
+        if file_content.count(char) == 0:
+            del apps_count[char]
+            break
     return apps_count
+
+
+
+def sort(my_dict):
+    apps_count_sort = dict(sorted(my_dict.items(), key=lambda item: item[1], reverse=True))
+    return apps_count_sort
+
+
+#sorted_dict_values = dict(sorted(my_dict.items(), key=lambda item: item[1]))
+
+    
